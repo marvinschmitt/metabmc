668221c3f29bd59c307c9b0e15f317132e5ec1ab
1129hiki <hikitaro2911@gmail.com>
2023-10-10 13:55:45 +0900

    functional documentation added

--- R/level3.R
+++ R/level3.R
@@ -1,15 +1,22 @@
+#' Obtain meta_uncertainty_fit object given observed and simulated posterior model probability.
+#'
+#' @param pmp_obs vector of observed model probability.
+#' @param pmp_sim dataframe containing index of true model in the first column and the pmp corresponding to each models.
+#' @param n_posterior_draws number of posterior draws after warm-up
+#' @param eps small number to be added / subtracted when posterior model probability takes 0 / 1
+#' @return meta_uncertainty object
+#' @export
 level_3 <- function(pmp_obs, pmp_sim, n_posterior_draws=1000, eps=1e-10){
   # given observed pmp and simulated pmp, do level 3.
   pmp_sim$pmp <- with(pmp_sim, as.matrix(pmp_sim[, colnames(pmp_sim)[3:ncol(pmp_sim)]]))
   ## if pmp include zero, adjust values
   pmp_sim$pmp <- avoid_error_by_zero_one(pmp_sim$pmp, eps)
 
-  print("Start fitting meta model")
+  print("Fitting meta model")
   meta_model_posteriors <- get_meta_model_posteriors(pmp_sim, n_posterior_draws = n_posterior_draws)
   meta_model_param <- extract_meta_model_param(meta_model_posteriors)
 
   # Predictive mixture
-  print("Start obtaining predictive mixture distribution")
   mixture_function <- create_mixture_function(pmp_obs, meta_model_param)
 
   out <- list(pmp_sim = pmp_sim, simulated_data_matrix = NULL, meta_model_param = meta_model_param, pmp_obs = pmp_obs, mixture_function = mixture_function)
