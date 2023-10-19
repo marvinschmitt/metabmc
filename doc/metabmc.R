1bb8c63f6b2fccb814e814ff93a8c338a2184c67
1129hiki <hikitaro2911@gmail.com>
2023-10-19 20:01:33 +0200

    vignette typo fixed and re-run

--- doc/metabmc.R
+++ doc/metabmc.R
@@ -30,15 +30,15 @@ m1 <- bf(x ~ 0 + arma(p=1, q=0))
 m2 <- bf(x ~ 0 + arma(p=0, q=1))
 m3 <- bf(x ~ 0 + arma(p=1, q=1))
 
-prior1 <- c(set_prior("normal(0, .2)", class = "ar"),
+prior1 <- c(set_prior("normal(0, 0.2)", class = "ar"),
+           set_prior("normal(0, 0.2)", class = "sigma"))
+
+prior2 <- c(set_prior("normal(0, 0.2)", class = "ma"),
            set_prior("normal(0, .2)", class = "sigma"))
 
-prior2 <- c(set_prior("normal(0, .2)", class = "ma"),
-           set_prior("normal(0, .2)", class = "sigma"))
-
-prior3 <- c(set_prior("normal(0, .2)", class = "ar"),
-           set_prior("normal(0, .2)", class = "ma"),
-           set_prior("normal(0, .2)", class = "sigma"))
+prior3 <- c(set_prior("normal(0, 0.2)", class = "ar"),
+           set_prior("normal(0, 0.2)", class = "ma"),
+           set_prior("normal(0, 0.2)", class = "sigma"))
 
 fit1 <- brm(m1, data=LakeHuron, prior = prior1, save_pars = save_pars(all = TRUE))
 fit2 <- brm(m2, data=LakeHuron, prior = prior2, save_pars = save_pars(all = TRUE))
@@ -47,7 +47,7 @@ fit3 <- brm(m3, data=LakeHuron, prior = prior3, save_pars = save_pars(all = TRUE
 ## ---- results='hide'--------------------------------------------------------------------
 metabmc_fit <- metabmc(fit1, fit2, fit3, n_sim=20)
 
-## ----eval=FALSE-------------------------------------------------------------------------
+## ---- eval=FALSE------------------------------------------------------------------------
 #  metabmc_fit <- metabmc(list(m1, m2, m3), list(prior1, prior2, prior3), data = LakeHuron, n_sim=20)
 
 ## ---------------------------------------------------------------------------------------
